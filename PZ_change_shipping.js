02b0fb9d4339cec40f016a3cbeca5f9fb96d9d1e
PZawadzki <p.zawadzki1405@gmail.com>
2022-07-29 17:55:15 -0500

    inventory counting - pick task search, movements search, inventory search

--- PZ_change_shipping.js
+++ PZ_change_shipping.js
@@ -28,31 +28,41 @@ define(['N/ui/serverWidget', 'N/search', 'N/https', 'N/ui/message', 'N/record', 
 					var orders = form.addField({
 						id: 'custpage_order_id',
 						type: serverWidget.FieldType.TEXTAREA,
-						label: 'Orders: '
+						label: 'Pick tass low quantity: '
 					});
 					orders.updateDisplayType({
 						displayType: serverWidget.FieldDisplayType.INLINE
 					});
 
+					var movements = form.addField({
+						id: 'custpage_movements_id',
+						type: serverWidget.FieldType.TEXTAREA,
+						label: 'Movements: '
+					});
+					movements.updateDisplayType({
+						displayType: serverWidget.FieldDisplayType.INLINE
+					});
+
+					//PICK TASK LOW QUANTITY
+
 					var strOutput = "";
 					try{
-						var mySearch = search.load({ id: 'customsearch5235'});
-						var results_search = mySearch.run();
-						var results_search_array = results_search.getRange({
+						var picktaskSearch = search.load({ id: 'customsearch4892'});
+						var results_picktask_search = picktaskSearch.run();
+						var results_picktask_search_array = results_picktask_search.getRange({
 							start: 0,
 							end: 20
 						});
 
-						log.debug("results_search_array.length -->: ", results_search_array.length);
+
+						log.debug("results_picktask_search_array.length -->: ", results_picktask_search_array.length);
 
 						//FEDEX HOME VAL = 3, USPS PRIORITY = 54
 
-						for (var i = 0; i < results_search_array.length; i++){
-								strOutput = 'Document Nr: ' + results_search_array[i].getValue(results_search.columns[0]) + ' iternal Id: ' +
-								results_search_array[i].getValue(results_search.columns[1]) +' Item: ' +
-								results_search_array[i].getText(results_search.columns[2]) +' Ship Via(Text): ' +
-								results_search_array[i].getText(results_search.columns[3]) +' Ship Via(Value): ' +
-								results_search_array[i].getValue(results_search.columns[3]) +'\n';
+						for (var i = 0; i < results_picktask_search_array.length; i++){
+								strOutput += 'Item: ' + results_picktask_search_array[i].getValue(results_picktask_search.columns[0]) + ' Bin: ' +
+								results_picktask_search_array[i].getText(results_picktask_search.columns[1]) +' Location avaible: ' +
+								results_picktask_search_array[i].getValue(results_picktask_search.columns[2]) +'\n';
 						}
 
 						if (strOutput == ''){
@@ -60,11 +70,65 @@ define(['N/ui/serverWidget', 'N/search', 'N/https', 'N/ui/message', 'N/record', 
 						}
 					}
 					catch (err){
-						 log.error("Error in USPS REAR BUMBER SEARCH", err);
+						 log.error("Error in pick task low quantity", err);
 					}
-
 					form.updateDefaultValues({
 						custpage_order_id: strOutput
+					});
+
+					///MOVEMENTS
+
+					var movementsOutput = "";
+					try{
+						var movementsSearch = search.load({ id: 'customsearch5382'});
+						var results_movements_search = movementsSearch.run();
+						var results_movements_search_array = results_movements_search.getRange({
+							start: 0,
+							end: 20
+						});
+
+						log.debug("results_movements_search_array.length -->: ", results_movements_search_array.length);
+						var itemName = "";
+						var itemAvaible = "";
+						for (var i = 0; i < results_movements_search_array.length; i++){
+
+							var inventorySearch = search.load({ id: 'customsearch5388'});
+							var inventoryFilters = inventorySearch.filters;
+							itemName = results_movements_search_array[i].getText(results_movements_search.columns[2]);
+							itemBin = results_movements_search_array[i].getValue(results_movements_search.columns[4]);
+							inventoryFilters.push(search.createFilter({ name: 'name', operator: search.Operator.IS, values: itemName}));
+							inventoryFilters.push(search.createFilter({ name: 'formulatext', operator: search.Operator.IS, values: itemBin, formula: "{binonhand.binnumber}"}));
+							inventorySearch.filters = inventoryFilters;
+							var results_inventory_search = inventorySearch.run();
+							var results_inventory_search_array = results_inventory_search.getRange({ start: 0, end: 10});
+							log.debug("results_inventory_search_array.length -->: ", results_inventory_search_array);
+							if (results_inventory_search_array.length == 0){ itemAvaible = 0; }
+							else{ itemAvaible = results_inventory_search_array[0].getValue(results_inventory_search.columns[3]);}
+							log.debug("itemAvaible -->: ", itemAvaible);
+
+							if (results_movements_search_array[i].getValue(results_movements_search.columns[3]) != itemAvaible){
+								movementsOutput += 'DOC: ' + results_movements_search_array[i].getValue(results_movements_search.columns[0]) + ' DATE: ' +
+								results_movements_search_array[i].getValue(results_movements_search.columns[1]) +' ITEM: ' +
+								results_movements_search_array[i].getText(results_movements_search.columns[2]) +' SNAPSHOT: ' +
+								results_movements_search_array[i].getValue(results_movements_search.columns[3]) +' AVAIBLE: ' +
+								itemAvaible + '\n';
+							}
+
+						}
+						for (var i = 0; i < results_movements_search_array.length; i++){
+
+					}
+
+						if (movementsOutput == ''){
+							movementsOutput = 'NOTHING';
+						}
+
+					}
+					catch (err){
+						 log.error("Error in movements", err);
+					}
+					form.updateDefaultValues({
+						custpage_movements_id: movementsOutput
 					});
 
 					var clientScriptInternalId = returnClientScriptInternalId();
