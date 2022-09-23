88f6c74068dc5151ad3ef72982bd996e6fd284b5
PZawadzki <p.zawadzki1405@gmail.com>
2022-09-23 11:12:23 -0500

    loading list for best seller and bins

--- PZ_Replenishment.js
+++ PZ_Replenishment.js
@@ -3,6 +3,10 @@
  * @NScriptType Suitelet
  * @NModuleScope SameAccount
  */
+
+var best_seller_search_id = 'customsearch4887';
+var bin_search_id = 'customsearch5128';
+
 define(['N/ui/serverWidget', 'N/search', 'N/https', 'N/ui/message', 'N/record', 'N/runtime'],
 
 	function(serverWidget, search, https, message, record, runtime) {
@@ -14,126 +18,57 @@ define(['N/ui/serverWidget', 'N/search', 'N/https', 'N/ui/message', 'N/record', 
 				var title = " onRequest ";
 
 					var itemName = context.request.parameters.itemName;
+					var typeVal = context.request.parameters.typeVal;
+
 					var form = serverWidget.createForm({
-						title: 'RMA'
+						title: 'Replenishments'
 					});
 
 
-					form.addField({
-						id: 'custpage_itemname_filter',
-						type: serverWidget.FieldType.TEXT,
-						label: 'ITEM NAME'
+					var typeFieldObj = form.addField({
+						id: 'custpage_type',
+						type: serverWidget.FieldType.SELECT,
+						label: 'Type'
 					});
-					form.updateDefaultValues({
-						custpage_itemname_filter: itemName
+					typeFieldObj.addSelectOption({
+						value: 'best_seller',
+						text: 'best_seller'
 					});
-					var rmaList = form.addSublist({
-						id: 'custpage_rmalist',
-						type: serverWidget.SublistType.LIST,
-						label: 'RMA'
+					typeFieldObj.addSelectOption({
+						value: 'bins',
+						text: 'bins'
 					});
-					rmaList.addField({
-						id: 'custpage_rmalist_checkbox',
-						type: serverWidget.FieldType.CHECKBOX,
-						label: 'CHECKBOX'
+					typeFieldObj.addSelectOption({
+						value: 'replenishments',
+						text: 'replenishments'
 					});
-					rmaList.addField({
-						id: 'custpage_rmalist_item',
-						type: serverWidget.FieldType.TEXT,
-						label: 'ITEM NAME'
-					});
-					rmaList.addField({
-						id: 'custpage_rmalist_item_id',
-						type: serverWidget.FieldType.TEXT,
-						label: 'ID'
-					}).updateDisplayType({
-						displayType: serverWidget.FieldDisplayType.HIDDEN
-					});
-					rmaList.addField({
-						id: 'custpage_rmalist_item_display',
-						type: serverWidget.FieldType.TEXT,
-						label: 'Display name'
-					});
-					rmaList.addField({
-						id: 'custpage_rmalist_available',
-						type: serverWidget.FieldType.TEXT,
-						label: 'AVAILABLE'
-					});
-					rmaList.addField({
-						id: 'custpage_rmalist_date',
-						type: serverWidget.FieldType.TEXT,
-						label: 'Last time received'
-					});
-					rmaList.addField({
-						id: 'custpage_rmalist_print',
-						type: serverWidget.FieldType.TEXT,
-						label: 'Print'
-					});
-					try{
-						var rmaSearch = search.load({ id: 'customsearch5453'});
-						if (itemName){
-							var rmafilters = rmaSearch.filters;
-							rmafilters.push(search.createFilter({name: 'name', operator: search.Operator.CONTAINS, values: itemName }));
-							rmaSearch.filters = rmafilters;
+					if (typeVal){
+						form.updateDefaultValues({
+							custpage_type: typeVal
+						});
+						switch(typeVal){
+							case 'best_seller':
+							form = best_sellerForm(form);
+							break;
+							case 'bins':
+							form = binsForm(form);
+							break;
+							case 'replenishments':
+							form = replenishmentForm(form);
+							break;
+							default:
+							//error message
+							break;
+
 						}
-						var results_rmaSearch = rmaSearch.run();
-						var results_rmaSearch_array = results_rmaSearch.getRange({
-							start: 0,
-							end: 100
-						});
-
-						//log.debug("results_stagingSearch -> ", results_stagingSearch);
-						//log.debug("results_stagingSearch_array -> ", results_stagingSearch_array);
-						log.debug("results_stagingSearch_array.lenght -> ", results_rmaSearch_array.length);
-
-					for (var i=0;  i < results_rmaSearch_array.length; i++){
-						rmaList.setSublistValue({
-							id: 'custpage_rmalist_item',
-							line: i,
-							value: results_rmaSearch_array[i].getValue(results_rmaSearch.columns[0])
-						});
-						rmaList.setSublistValue({
-							id: 'custpage_rmalist_item_id',
-							line: i,
-							value: results_rmaSearch_array[i].getValue(results_rmaSearch.columns[1])
-						});
-						rmaList.setSublistValue({
-							id: 'custpage_rmalist_item_display',
-							line: i,
-							value: results_rmaSearch_array[i].getValue(results_rmaSearch.columns[2])
-						});
-						rmaList.setSublistValue({
-							id: 'custpage_rmalist_available',
-							line: i,
-							value: results_rmaSearch_array[i].getValue(results_rmaSearch.columns[3])
-						});
-						rmaList.setSublistValue({
-							id: 'custpage_rmalist_date',
-							line: i,
-							value: results_rmaSearch_array[i].getValue(results_rmaSearch.columns[4])
-						});
-						rmaList.setSublistValue({
-							id: 'custpage_rmalist_print',
-							line: i,
-							value: '<a href="https://6241176.app.netsuite.com/app/site/hosting/scriptlet.nl?script=726&deploy=1&compid=6241176&item_id='+
-							results_rmaSearch_array[i].getValue(results_rmaSearch.columns[1])+
-							'&item_type=InvtPart" target="_blank">Print Label</a>'
-						});
 					}
-
-
-					}
-					catch(err){
-						log.error("error in loading RMA list");
-					}
-
 					var clientScriptInternalId = returnClientScriptInternalId();
 					log.debug("clientScriptInternalId", clientScriptInternalId);
 
 					form.addButton({
-						id: 'custpage_transfer',
-						label: 'Remove from RMA',
-						functionName: 'removeRMA()'
+						id: 'custpage_submit',
+						label: 'Submit',
+						functionName: 'submit_button()'
 					});
 					form.addButton({
 						id: 'custpage_transfer',
@@ -152,7 +87,258 @@ define(['N/ui/serverWidget', 'N/search', 'N/https', 'N/ui/message', 'N/record', 
 
 		}
 
+		function best_sellerForm(form){
+			form.addField({
+				id: 'custpage_label',
+				type: serverWidget.FieldType.LABEL,
+				label: 'this is best_seller'
+			});
+			var bestsellerList = form.addSublist({
+				id: 'custpage_bestlist',
+				type: serverWidget.SublistType.LIST,
+				label: 'Best sellers'
+			});
+			bestsellerList.addField({
+				id: 'custpage_bestlist_checkbox',
+				type: serverWidget.FieldType.CHECKBOX,
+				label: 'CHECKBOX'
+			});
+			bestsellerList.addField({
+				id: 'custpage_bestlist_item',
+				type: serverWidget.FieldType.TEXT,
+				label: 'ITEM'
+			});
+			bestsellerList.addField({
+				id: 'custpage_bestlist_item_id',
+				type: serverWidget.FieldType.TEXT,
+				label: 'ITEM ID'
+			});
+			bestsellerList.addField({
+				id: 'custpage_bestlist_display',
+				type: serverWidget.FieldType.TEXT,
+				label: 'DISPLAY NAME'
+			});
+			bestsellerList.addField({
+				id: 'custpage_bestlist_picked',
+				type: serverWidget.FieldType.TEXT,
+				label: 'PICKED'
+			});
+			bestsellerList.addField({
+				id: 'custpage_bestlist_recomended',
+				type: serverWidget.FieldType.TEXT,
+				label: 'RECOMENDED BIN'
+			});
+			try{
+			var best_sellerSearch = search.load({ id: 'customsearch4887'});
 
+			// var bestfilters = best_sellerSearch.filters;
+			// bestfilters.push(search.createFilter({name: 'preferredbin', join: 'item', operator: search.Operator.IS, values: true }));
+			// best_sellerSearch.filters = bestfilters;
+
+			var results_best_sellerSearch = best_sellerSearch.run();
+			var results_best_sellerSearch_array = results_best_sellerSearch.getRange({
+				start: 0,
+				end: 20
+			});
+
+			log.debug("results_best_sellerSearch_array -> ", results_best_sellerSearch_array);
+
+			log.debug("results_stagingSearch_array.lenght -> ", results_best_sellerSearch_array.length);
+			for (var i=0;  i < results_best_sellerSearch_array.length; i++){
+				bestsellerList.setSublistValue({
+					id: 'custpage_bestlist_item',
+					line: i,
+					value: results_best_sellerSearch_array[i].getText(results_best_sellerSearch.columns[0])
+				});
+				bestsellerList.setSublistValue({
+					id: 'custpage_bestlist_item_id',
+					line: i,
+					value: results_best_sellerSearch_array[i].getValue(results_best_sellerSearch.columns[0])
+				});
+				bestsellerList.setSublistValue({
+					id: 'custpage_bestlist_display',
+					line: i,
+					value: results_best_sellerSearch_array[i].getValue(results_best_sellerSearch.columns[1])
+				});
+				bestsellerList.setSublistValue({
+					id: 'custpage_bestlist_picked',
+					line: i,
+					value: results_best_sellerSearch_array[i].getValue(results_best_sellerSearch.columns[3])
+				});
+				var recomended = results_best_sellerSearch_array[i].getValue(results_best_sellerSearch.columns[4]);
+				if (!recomended){
+					recomended = '01-01-A'
+				}
+				bestsellerList.setSublistValue({
+					id: 'custpage_bestlist_recomended',
+					line: i,
+					value: recomended
+				});
+			}
+
+			}
+			catch(e){
+				log.error("error in loading best seller list", e);
+			}
+			return form;
+		}
+
+		function binsForm(form){
+			form.addField({
+				id: 'custpage_label',
+				type: serverWidget.FieldType.LABEL,
+				label: 'this is bins'
+			});
+
+			form.addField({
+				id: 'custpage_bin_min',
+				type: serverWidget.FieldType.TEXT,
+				label: 'BIN MINIMUM'
+			});
+
+			form.addField({
+				id: 'custpage_bin_max',
+				type: serverWidget.FieldType.TEXT,
+				label: 'BIN MAXIMUM'
+			});
+
+			form.addField({
+				id: 'custpage_bin_replen',
+				type: serverWidget.FieldType.TEXT,
+				label: 'BIN REPLEN'
+			});
+
+			form.addField({
+				id: 'custpage_bin_round',
+				type: serverWidget.FieldType.TEXT,
+				label: 'BIN ROUND'
+			});
+
+			var binsList = form.addSublist({
+				id: 'custpage_binslist',
+				type: serverWidget.SublistType.LIST,
+				label: 'BIN LIST'
+			});
+			binsList.addField({
+				id: 'custpage_binslist_checkbox',
+				type: serverWidget.FieldType.CHECKBOX,
+				label: 'CHECKBOX'
+			});
+			binsList.addField({
+				id: 'custpage_binslist_item',
+				type: serverWidget.FieldType.TEXT,
+				label: 'ITEM'
+			});
+			binsList.addField({
+				id: 'custpage_binslist_bin',
+				type: serverWidget.FieldType.TEXT,
+				label: 'BIN'
+			});
+			binsList.addField({
+				id: 'custpage_binslist_bin_id',
+				type: serverWidget.FieldType.TEXT,
+				label: 'BIN ID'
+			});
+			binsList.addField({
+				id: 'custpage_binslist_min',
+				type: serverWidget.FieldType.TEXT,
+				label: 'MIN'
+			});
+			binsList.addField({
+				id: 'custpage_binslist_max',
+				type: serverWidget.FieldType.TEXT,
+				label: 'MAX'
+			});
+			binsList.addField({
+				id: 'custpage_binslist_replen',
+				type: serverWidget.FieldType.TEXT,
+				label: 'RPLN'
+			});
+			binsList.addField({
+				id: 'custpage_binslist_round',
+				type: serverWidget.FieldType.TEXT,
+				label: 'ROUND'
+			});
+			try{
+			var binSearch = search.load({ id: 'customsearch5128'});
+
+			// var bestfilters = best_sellerSearch.filters;
+			// bestfilters.push(search.createFilter({name: 'preferredbin', join: 'item', operator: search.Operator.IS, values: true }));
+			// best_sellerSearch.filters = bestfilters;
+
+			var results_binSearch = binSearch.run();
+			var results_binSearch_array = results_binSearch.getRange({
+				start: 0,
+				end: 20
+			});
+
+			log.debug("results_binSearch_array", results_binSearch_array);
+			log.debug("results_binSearch_array length", results_binSearch_array.length);
+
+
+			for (var i=0;  i < results_binSearch_array.length; i++){
+				binsList.setSublistValue({
+					id: 'custpage_binslist_item',
+					line: i,
+					value: results_binSearch_array[i].getValue(results_binSearch.columns[0])
+				});
+				//log.debug("wartosc1 ", results_binSearch_array[i].getValue(results_binSearch.columns[0]));
+				binsList.setSublistValue({
+					id: 'custpage_binslist_bin',
+					line: i,
+					value: results_binSearch_array[i].getValue(results_binSearch.columns[1])
+				});
+				binsList.setSublistValue({
+					id: 'custpage_binslist_bin_id',
+					line: i,
+					value: results_binSearch_array[i].getValue(results_binSearch.columns[2])
+				});
+				var bin_min = results_binSearch_array[i].getValue(results_binSearch.columns[3]);
+				if (!bin_min) bin_min = 0;
+				var bin_max = results_binSearch_array[i].getValue(results_binSearch.columns[4]);
+				if (!bin_max) bin_max = 0;
+				var bin_rpln = results_binSearch_array[i].getValue(results_binSearch.columns[5]);
+				if (!bin_rpln) bin_rpln = 0;
+				var bin_round = results_binSearch_array[i].getValue(results_binSearch.columns[6]);
+				if (!bin_round) bin_round = 0;
+				binsList.setSublistValue({
+					id: 'custpage_binslist_min',
+					line: i,
+					value: bin_min
+				});
+				binsList.setSublistValue({
+					id: 'custpage_binslist_max',
+					line: i,
+					value: bin_max
+				});
+				binsList.setSublistValue({
+					id: 'custpage_binslist_replen',
+					line: i,
+					value: bin_rpln
+				});
+				binsList.setSublistValue({
+					id: 'custpage_binslist_round',
+					line: i,
+					value: bin_round
+				});
+				//log.debug("wartosc2 ", results_binSearch_array[i].getValue(results_binSearch.columns[0]));
+				}
+			}
+			catch (e){
+				log.error("error in bin list ", e);
+			}
+
+			return form;
+		}
+
+		function replenishmentForm(form){
+			form.addField({
+				id: 'custpage_label',
+				type: serverWidget.FieldType.LABEL,
+				label: 'this is replenishments'
+			});
+			return form;
+		}
 
 		//	The function returns the search result array
 		function returnClientScriptInternalId() {
@@ -169,7 +355,7 @@ define(['N/ui/serverWidget', 'N/search', 'N/https', 'N/ui/message', 'N/record', 
 						name: 'name',
 						join: 'file',
 						operator: 'haskeywords',
-						values: ["PZ_RMA_func.js"]
+						values: ["PZ_Replenishment_func.js"]
 					}]
 				});
 
--- PZ_Replenishment_func.js
+++ PZ_Replenishment_func.js
@@ -9,224 +9,160 @@ var USA_subsidiary = 2;
 var USA_location = 3;
 var USA_rma_location = 6;
 var Germany_rma_location = 8;
+var scriptid_number = 'customscript3017';
+var deploymentid_number = 'customdeploy1';
 
 define(['N/url', 'N/currentRecord', 'N/record', 'N/search', 'N/https', 'N/ui/dialog'],
 	function(url, currentRecord, record, search, https, dialog) {
 
-		function removeRMA(){
+		function submit_button(){
 
 			try{
 
-				var subsidiaryId = USA_subsidiary;
-		    var locationId = USA_rma_location;
-				var accountId = 58;
-				//var itemId = 400;
-				//var itemQuantity = 1;
-				var adjBin = 'NEW RMA';
 				var currRec = currentRecord.get();
+				var typeVal = currRec.getValue({
+					fieldId: 'custpage_type'
+				});
+				switch(typeVal){
+					case 'best_seller':
+					best_button(currRec);
+					break;
+					case 'bins':
+					bin_button(currRec);
+					break;
+					case 'replenishments':
+
+					break;
+					default:
+					//error message
+					break;
+
+				}
+
+			}
+			catch(err){
+				log.error("Error in submit button", err);
+			}
+
+		}
+
+		function best_button(currRec){
+			try{
 				var numLines = currRec.getLineCount({
-						sublistId: 'custpage_rmalist'
-					});
+					sublistId: 'custpage_bestlist'
+				});
 
-
-				var rmaAdjustement = record.create({
-												type: record.Type.INVENTORY_ADJUSTMENT,
-												isDynamic: true
-				});
-				var date = new Date();
-			 rmaAdjustement.setValue({
-					 fieldId: 'trandate',
-					 value: date
-			 });
-				rmaAdjustement.setValue({
-						fieldId: 'subsidiary',
-						value: subsidiaryId
-				});
-				rmaAdjustement.setValue({
-						fieldId: 'location',
-						value: locationId
-				});
-				rmaAdjustement.setValue({
-						fieldId: 'adjlocation',
-						value: locationId
-				});
-				rmaAdjustement.setValue({
-						fieldId: 'account',
-						value: accountId
-				});
-				rmaAdjustement.setValue({
-						fieldId: 'memo',
-						value: 'RMA Adjustement'
-				});
-				// rmaAdjustement.setValue({
-				// 		fieldId: 'customform',
-				// 		value: 10
-				// });
-				for (var i=0; i < numLines; i++){
+				for (var i = 0; i < numLines; i++) {
 					var checkbox = currRec.getSublistValue({
-						sublistId: 'custpage_rmalist',
-						fieldId: 'custpage_rmalist_checkbox',
+						sublistId: 'custpage_bestlist',
+						fieldId: 'custpage_bestlist_checkbox',
 						line: i
 					});
 					if (checkbox){
 						var itemid = currRec.getSublistValue({
-							sublistId: 'custpage_rmalist',
-							fieldId: 'custpage_rmalist_item_id',
+							sublistId: 'custpage_bestlist',
+							fieldId: 'custpage_bestlist_item_id',
 							line: i
 						});
-						var item = currRec.getSublistValue({
-							sublistId: 'custpage_rmalist',
-							fieldId: 'custpage_rmalist_item',
+						var recomended = currRec.getSublistValue({
+							sublistId: 'custpage_bestlist',
+							fieldId: 'custpage_bestlist_recomended',
 							line: i
 						});
-						rmaAdjustement.selectNewLine({
-								sublistId: 'inventory'
+						// var id = record.submitFields({
+						// 	type: record.Type.INVENTORY_ITEM,
+						// 	id: itemid,
+						// 	values: {
+						// 		binnumber: recomended,
+						// 		preferredbin: true
+						// 	}
+						// });
+						var id = record.load({
+							type: record.Type.INVENTORY_ITEM,
+							id: 7045,
 						});
-						rmaAdjustement.setCurrentSublistValue({
-								sublistId: 'inventory',
-								fieldId: 'item',
-								value: itemid
-						});
-						rmaAdjustement.setCurrentSublistValue({
-								sublistId: 'inventory',
-								fieldId: 'location',
-								value: locationId
-						});
+						log.debug("id to jest ", id.binnumber);
 
-						var itemQuantity = prompt("How many of item "+item+" do you want to remove?");
-						var rmaQuantity = currRec.getSublistValue({
-							sublistId: 'custpage_rmalist',
-							fieldId: 'custpage_rmalist_available',
-							line: i
-						});
-						if (itemQuantity <=0){
-							alert("You entered wrong number");
-							continue;
-						}
-						if (itemQuantity > rmaQuantity){
-							alert("There is not enough items "+item+" in RMA");
-							continue;
-						}
-						else{
-							itemQuantity = itemQuantity*(-1);
-						}
-						rmaAdjustement.setCurrentSublistValue({
-								sublistId: 'inventory',
-								fieldId: 'adjustqtyby',
-								value: itemQuantity
-						});
-						var reason = prompt("Why do you want to remove item "+item+"?");
-						rmaAdjustement.setCurrentSublistValue({
-								sublistId: 'inventory',
-								fieldId: 'memo',
-								value: reason
-						});
-						var rmaInventoryDetail = rmaAdjustement.getCurrentSublistSubrecord({
-								sublistId: 'inventory',
-								fieldId: 'inventorydetail'
-						});
-						rmaInventoryDetail.selectNewLine({
-								sublistId: 'inventoryassignment'
-						});
-						rmaInventoryDetail.setCurrentSublistValue({
-						    sublistId: 'inventoryassignment',
-						     fieldId: 'issueinventorynumber',
-						     value: 'lotNumber'
-						});
-						rmaInventoryDetail.setCurrentSublistText({
-								sublistId: 'inventoryassignment',
-								fieldId: 'binnumber',
-								text: adjBin
-						});
-						rmaInventoryDetail.setCurrentSublistValue({
-								sublistId: 'inventoryassignment',
-								fieldId: 'quantity',
-								value: itemQuantity
-						});
-						rmaInventoryDetail.commitLine({
-								sublistId: 'inventoryassignment'
-						});
-						//log.debug("commited inventory detail" , rmaInventoryDetail);
-						rmaAdjustement.commitLine({
-								sublistId: 'inventory'
-						});
-						//log.debug("commited inventory adjust" , rmaAdjustement);
 					}
 				}
-				//
+			}
+			catch(e){
+				log.error("error in best button ", e);
+			}
 
+		}
 
+		function bin_button(currRec){
 
+			try{
+			var numLines = currRec.getLineCount({
+				sublistId: 'custpage_binslist'
+			});
 
-				//
+			for (var i = 0; i < numLines; i++) {
+				var checkbox = currRec.getSublistValue({
+					sublistId: 'custpage_binslist',
+					fieldId: 'custpage_binslist_checkbox',
+					line: i
+				});
+				if (checkbox){
+					var bin_id = currRec.getSublistValue({
+						sublistId: 'custpage_binslist',
+						fieldId: 'custpage_binslist_bin_id',
+						line: i
+					});
+					var bin_min = currRec.getValue({
+							fieldId: 'custpage_bin_min'
+					});
+					var bin_max = currRec.getValue({
+							fieldId: 'custpage_bin_max'
+					});
+					var bin_rpln = currRec.getValue({
+							fieldId: 'custpage_bin_replen'
+					});
+					var bin_round = currRec.getValue({
+							fieldId: 'custpage_bin_round'
+					});
+					var id = record.submitFields({
+						type: record.Type.BIN,
+						id: bin_id,
+						values: {
+							// custrecord_wmsse_replen_roundqty: bin_round,
+							// custrecord_wmsse_replen_qty: bin_rpln,
+							// custrecord_wmsse_replen_minqty: bin_min,
+							custrecord_wmsse_replen_maxqty: bin_max
 
-
-				try {
-						var linesRmaRemove = rmaAdjustement.getLineCount({sublistId: 'inventory'});
-						if (linesRmaRemove > 0){
-							var recId = rmaAdjustement.save();
-							log.debug({
-									title: 'Record created successfully',
-									details: 'Id: ' + recId
-								});
-								alert("You successfully removed items from RMA");
-								resetPage();
 						}
-						else {
-							alert("You need to select items to remove");
-							resetPage();
-						}
-				} catch (e) {
-						log.error("Error in transferStaging", e);
+					});
+					log.debug("id to jest ", id);
+
 				}
-
-				//	var response = prompt(" Please enter something ");
-				//	alert("You enetered "+response);
 			}
-			catch(err){
-				log.error("Error in transferStaging", err);
-			}
-
+		}
+		catch(e){
+			console.log("error in bin button ", e);
+			log.error("error in bin button", e);
+		}
 		}
 
 		function pageInit(scriptContext) {}
 
 		function fieldChanged(scriptContext) {
 			try{
-			if (scriptContext.fieldId == 'custpage_itemname_filter'){
-				var currRec = scriptContext.currentRecord;
-				var itemName = currRec.getValue({
-					fieldId: 'custpage_itemname_filter'
-				});
-				var suiteletLink = url.resolveScript({
-					scriptId: 'customscript3261',
-					deploymentId: 'customdeploy1'
-				});
-				suiteletLink += '&itemName=' + itemName;
-				window.onbeforeunload = null;
-				window.location.href = suiteletLink;
-		}
+		if (scriptContext.fieldId == 'custpage_type'){
+			var currRec = scriptContext.currentRecord;
+			var typeVal = currRec.getValue({
+				fieldId: 'custpage_type'
+			});
+			var suiteletLink = url.resolveScript({
+				scriptId: scriptid_number,
+				deploymentId: deploymentid_number
+			});
+			suiteletLink += '&typeVal=' + typeVal;
+			window.onbeforeunload = null;
+			window.location.href = suiteletLink;
+	}
 
-		// if (scriptContext.sublistId == 'custpage_rmalist' && scriptContext.fieldId == 'custpage_rmalist_checkbox'){
-		// 		var currRec = scriptContext.currentRecord;
-		// 	var currIndex = currRec.getCurrentSublistIndex({sublistId: 'custpage_rmalist'});
-		// 	var numLines = currRec.getLineCount({
-		// 		sublistId: 'custpage_rmalist'
-		// 	});
-		// 	console.log("currIndex", currIndex);
-		// 	console.log("numLines", numLines);
-		// 	for (var i=0; i < numLines; i++){
-		// 		if (i != currIndex){
-		// 			// currRec.setCurrentSublistValue({
-		// 			// 	sublistId : 'custpage_rmalist',
-		// 			// 	fieldId : 'custpage_rmalist_checkbox',
-		// 			// 	line: i,
-		// 			// 	value: false
-		// 			// });
-		//
-		// 		}
-		// 	}
-		// }
 	}
 	catch(error){
 		//alert("Error in field changed", error);
@@ -237,42 +173,19 @@ define(['N/url', 'N/currentRecord', 'N/record', 'N/search', 'N/https', 'N/ui/dia
 		}
 
 		function resetPage(){
-			// try{
-			// var currRec = currentRecord.get();
-			// var numLines = currRec.getLineCount({
-			// 		sublistId: 'custpage_rmalist'
-			// 	});
-			//
-			// 	for (var i=0; i < numLines; i++){
-			// 		var checkbox = currRec.getSublistValue({
-			// 			sublistId: 'custpage_rmalist',
-			// 			fieldId: 'custpage_rmalist_checkbox',
-			// 			line: i
-			// 		});
-			// 		//log.debug("checkbox ", i, " value ", checkbox);
-			// 		if (checkbox){
-			// 			var item = currRec.getSublistValue({
-			// 				sublistId: 'custpage_rmalist',
-			// 				fieldId: 'custpage_rmalist_item',
-			// 				line: i
-			// 			});
-			// 			alert("you selected item"+item);
-			// 		}
-			// 	}
-			// }
-			// catch(e){
-			// 	log.error("error in reset page",e);
-			// }
+
 			var suiteletLink = url.resolveScript({
-				scriptId: 'customscript3261',
-				deploymentId: 'customdeploy1'
+				scriptId: scriptid_number,
+				deploymentId: deploymentid_number
 			});
 			window.onbeforeunload = null;
 			window.location.href = suiteletLink;
 		}
 
 		return {
-			removeRMA: removeRMA,
+			submit_button: submit_button,
+			best_button:best_button,
+			bin_button:bin_button,
 			fieldChanged:fieldChanged,
 			resetPage:resetPage,
 			pageInit: pageInit
